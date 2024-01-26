82e33138e66ec68466fe229c4a25e9f4ab3cf987
purepennons <y80811@gmail.com>
2024-01-25 23:05:50 -0800

    chore($package): add highcharts

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import "bootstrap/dist/css/bootstrap.css";
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import HighchartsVue from 'highcharts-vue'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+app.use(HighchartsVue)
 app.use(createPinia())
 app.use(router)
 
