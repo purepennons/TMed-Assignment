a220d6a454b663a49505aa5dc400a71dcd139e6f
purepennons <y80811@gmail.com>
2024-01-25 20:29:15 -0800

    build: init the project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <header>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+    <RouterLink to="/dashboard">Dashboard</RouterLink>
+  </header>
+
+  <RouterView />
+</template>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/views/DashboardView.vue
@@ -0,0 +1,22 @@
+<script>
+</script>
+
+<template>
+    <div class="container">
+        <div class="row">
+            <nav class="col-sm-6 col-lg-4">nav</nav>
+            <main class="col-sm-6 col-lg-8">main</main>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    nav {
+        height: 200px;
+        background: yellow;
+    }
+
+    main {
+        background: tomato;
+    }
+</style>
