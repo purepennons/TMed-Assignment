055fa09ae0d94cc276bb93ab38b36778af10a9fc
purepennons <y80811@gmail.com>
2024-01-27 03:09:32 -0800

    feat($dashboard): make real-time chart to allow to switch datasets

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,35 +1,77 @@
 <script>
-import { subHours, addHours, startOfDay, getTime, addDays } from 'date-fns/fp'
-import { pipe } from 'lodash/fp'
+import { subHours, addHours, startOfDay, getTime, addDays, format } from 'date-fns/fp'
+import { pipe, map, defaultsDeep } from 'lodash/fp'
 
 import { toUTCDate } from '@/utils'
-import { TIME_ZONE } from '@/constants'
+import { TIME_ZONE, DATE_STRING_FORMAT } from '@/constants'
 
 import useTimeData from '@/data/use_time.json'
-import realTimeData from '@/data/real_time_20240116.json'
+import realTimeData20240116 from '@/data/real_time_20240116.json'
+import realTimeData20240118 from '@/data/real_time_20240118.json'
 
 function getDateTimeBoundaries(date) {
     const period = 12 // hours
 
-    const baseDate = pipe(
-        toUTCDate,
-        startOfDay,
-        addDays(1),
-    )(date)
+    const baseDate = pipe(toUTCDate, startOfDay, addDays(1))(date)
 
     return [getTime(subHours(period)(baseDate)), getTime(addHours(period)(baseDate))]
 }
 
-const selectedDateString = '2024-01-16 00:00:00.000'
-const [initialRealTimeMinDateTime, initialRealTimeMaxDateTime] = getDateTimeBoundaries(selectedDateString)
-console.log(initialRealTimeMinDateTime, initialRealTimeMaxDateTime)
-console.log('2024-01-17 00:06:02.000', toUTCDate('2024-01-17 00:06:02.000'))
+const normalizeRealTimeData = map(([x, y]) => [toUTCDate(x), y])
+
+const realTimeDataMap = {
+    '2024-01-16 00:00:00.000': realTimeData20240116,
+    '2024-01-18 00:00:00.000': realTimeData20240118
+}
+
+const defaultLargePointChartOptions = () => ({
+    chart: {
+        type: 'line',
+        zoomType: 'x'
+    },
+    title: {
+        text: 'PulseRate',
+        align: 'left'
+    },
+    xAxis: {
+        type: 'datetime',
+        tickInterval: 3600 * 1000
+    },
+    yAxis: {
+        title: {
+            text: 'Pulse'
+        },
+        tickInterval: 25
+    },
+    legend: {
+        enabled: false
+    },
+    time: {
+        timezone: TIME_ZONE
+    },
+    series: [
+        {
+            name: 'Pulse',
+            data: []
+        }
+    ]
+})
 
 export default {
     name: 'DashboardView',
     data() {
+        const realTimeSelectedDate = format(DATE_STRING_FORMAT, startOfDay(new Date()))
+        const defaultRealTimeData = realTimeDataMap[realTimeSelectedDate] ?? { data: [] }
+        let initialRealTimeMinDateTime = undefined 
+        let initialRealTimeMaxDateTime = undefined
+
+        if (defaultRealTimeData) {
+            [initialRealTimeMinDateTime, initialRealTimeMaxDateTime] = getDateTimeBoundaries(
+                realTimeSelectedDate
+            )
+        }
+
         return {
-            selectedDateString,
             columnChartOptions: {
                 chart: {
                     type: 'column'
@@ -58,39 +100,40 @@ export default {
                     }
                 ]
             },
-            largePointChartOptions: {
-                chart: {
-                    type: 'line',
-                    zoomType: 'x',
-                },
-                title: {
-                    text: 'PulseRate',
-                    align: 'left'
-                },
+            realTimeSelectedDate,
+            largePointChartOptions: defaultsDeep(defaultLargePointChartOptions())({
                 xAxis: {
-                    type: 'datetime',
                     min: initialRealTimeMinDateTime,
-                    max: initialRealTimeMaxDateTime,
-                    tickInterval: 3600 * 1000,
+                    max: initialRealTimeMaxDateTime
                 },
-                yAxis: {
-                    title: {
-                        text: 'Pulse'
-                    },
-                    min: 0,
-                    tickInterval: 25,
-                },
-                legend: {
-                    enabled: false
-                },
-                time: {
-                    timezone: TIME_ZONE
-                },
-                series: [{
-                    name: 'Pulse',
-                    data: realTimeData.data.map(([x, y]) => [toUTCDate(x), y]),
-                }]
+                series: [
+                    {
+                        data: normalizeRealTimeData(defaultRealTimeData.data)
+                    }
+                ]
+            })
+        }
+    },
+    computed: {},
+    methods: {
+        handleRealTimeDateChange() {
+            const dateString = format(DATE_STRING_FORMAT, this.realTimeSelectedDate)
+            const realTimeData = realTimeDataMap[dateString]
+
+            if (!realTimeData) {
+                this.largePointChartOptions = defaultLargePointChartOptions()
+            } else {
+                const [minDateTime, maxDateTime] = getDateTimeBoundaries(dateString)
+    
+                this.largePointChartOptions.xAxis.min = minDateTime
+                this.largePointChartOptions.xAxis.max = maxDateTime
+                this.largePointChartOptions.series[0].data = normalizeRealTimeData(realTimeData.data)
             }
+        }
+    },
+    watch: {
+        realTimeSelectedDate() {
+            this.handleRealTimeDateChange()
         }
     }
 }
@@ -102,7 +145,19 @@ export default {
             <nav class="col-lg-2">nav</nav>
             <main class="col-sm-12 col-lg-10">
                 <highcharts :options="columnChartOptions"></highcharts>
-                <highcharts :options="largePointChartOptions"></highcharts>
+                <div class="container">
+                    <VDatePicker v-model="realTimeSelectedDate" :popover="{ visibility: 'click' }">
+                        <template #default="{ inputValue, inputEvents }">
+                            <input
+                                type="text"
+                                :readonly="true"
+                                :value="inputValue"
+                                v-on="inputEvents"
+                            />
+                        </template>
+                    </VDatePicker>
+                    <highcharts :options="largePointChartOptions"></highcharts>
+                </div>
             </main>
         </div>
     </div>
