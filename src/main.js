245505411240cfd01acfcce80f3ff73c06543f37
purepennons <y80811@gmail.com>
2024-01-27 03:00:48 -0800

    chore($packages): add v-calendar

--- src/main.js
+++ src/main.js
@@ -1,17 +1,23 @@
-import "bootstrap/dist/css/bootstrap.css";
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import HighchartsVue from 'highcharts-vue'
+import { setupCalendar, Calendar, DatePicker } from 'v-calendar';
 
 import App from './App.vue'
 import router from './router'
 
+import "bootstrap/dist/css/bootstrap.css";
+import 'v-calendar/style.css';
+
 const app = createApp(App)
 
 app.use(HighchartsVue)
+app.use(setupCalendar, {})
 app.use(createPinia())
 app.use(router)
+
+app.component('VCalendar', Calendar)
+app.component('VDatePicker', DatePicker)
 
 app.mount('#app')
 
