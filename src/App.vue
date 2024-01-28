4db7ab04f4184aacbed40fea6ccacee22fee9108
purepennons <y80811@gmail.com>
2024-01-28 01:32:29 -0800

    refactor($router): remote unused routers

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,9 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 
 </script>
 
 <template>
-  <header>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/about">About</RouterLink>
-    <RouterLink to="/dashboard">Dashboard</RouterLink>
-  </header>
-
   <RouterView />
 </template>
 
