415921fc2ad1e64ca88167a08a194a4fd990d6ae
purepennons <y80811@gmail.com>
2024-01-28 02:52:55 -0800

    refactor($router): remove unused file and set the dashboard page as the home page

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import DashboardView from '@/views/DashboardView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/dashboard',
       name: 'dashboard',
-      component: () => import('../views/DashboardView.vue')
-    }
+      component: DashboardView
+    },
+    // {
+    //   path: '/dashboard',
+    //   name: 'dashboard',
+    //   component: () => import('../views/DashboardView.vue')
+    // }
   ]
 })
 
