10aa5b95ccda25f39c4b139a1bc035d1effdb4a2
purepennons <y80811@gmail.com>
2024-01-29 17:22:17 -0800

    refactor($styles): move the global styles into main.scss

--- src/App.vue
+++ src/App.vue
@@ -5,26 +5,3 @@ import { RouterView } from 'vue-router'
 <template>
     <RouterView />
 </template>
-
-<style lang="scss">
-// v-calendar overwrite
-.vc-calendar-date-picker {
-    * {
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-rendering: optimizeLegibility;
-    }
-
-    &.vc-container {
-        border-radius: 8px;
-        border-bottom: 3px solid #ededed;
-        background: white;
-        box-shadow: 0px 0px 0px 3px rgba(221, 221, 221, 0.16);
-    }
-
-    & .vc-title,
-    .vc-arrow {
-        background: white;
-    }
-}
-</style>
